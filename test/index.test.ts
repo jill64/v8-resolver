426a2b100239cbae1f789d9b021309120137e50e
jill64 <intents.turrets0h@icloud.com>
2024-02-09 15:54:30 +0900

    update test

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,6 +5,7 @@ import process from 'node:process'
 import v8 from 'node:v8'
 import { test } from 'vitest'
 import { NodeV8Coverage, resolve } from '../src/index'
+import { expect } from 'vitest'
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname)
 const out = path.resolve(__dirname, './demo/coverage')
@@ -46,4 +47,83 @@ test('integration', async () => {
     path.join(out, 'resolved.json'),
     JSON.stringify(resolved, null, 2)
   )
+
+  expect(resolved).toEqual([
+    {
+      scriptId: '10400',
+      url: 'file:///Users/jill/v8-resolver/test/demo/dist/bundle.js',
+      functions: [
+        {
+          functionName: '',
+          ranges: [
+            {
+              startOffset: 0,
+              endOffset: 129,
+              count: 1
+            }
+          ],
+          isBlockCoverage: true
+        }
+      ],
+      source:
+        'var e=(r,n)=>r+n;var u=(r,n)=>r-n;var s=r=>{let n=e(r,1);return u(n,1)};export{s as natural};\n//# sourceMappingURL=bundle.js.map\n'
+    },
+    {
+      url: 'file:///Users/jill/v8-resolver/test/demo/src/add.ts',
+      scriptId: '10410',
+      source:
+        'export const add = (a: number, b: number): number => {\n  return a + b\n}\n',
+      functions: [
+        {
+          functionName: 'e',
+          isBlockCoverage: true,
+          ranges: [
+            {
+              startOffset: 19,
+              endOffset: 68,
+              count: 1
+            }
+          ]
+        }
+      ]
+    },
+    {
+      url: 'file:///Users/jill/v8-resolver/test/demo/src/sub.ts',
+      scriptId: '10420',
+      source:
+        'export const sub = (a: number, b: number): number => {\n  return a - b\n}\n',
+      functions: [
+        {
+          functionName: 'u',
+          isBlockCoverage: true,
+          ranges: [
+            {
+              startOffset: 19,
+              endOffset: 68,
+              count: 1
+            }
+          ]
+        }
+      ]
+    },
+    {
+      url: 'file:///Users/jill/v8-resolver/test/demo/src/index.ts',
+      scriptId: '10430',
+      source:
+        "import { add } from './add'\nimport { sub } from './sub'\n\nexport const natural = (n: number): number => {\n  const res = add(n, 1)\n  return sub(res, 1)\n}\n",
+      functions: [
+        {
+          functionName: 's',
+          isBlockCoverage: true,
+          ranges: [
+            {
+              startOffset: 81,
+              endOffset: 150,
+              count: 1
+            }
+          ]
+        }
+      ]
+    }
+  ])
 })
