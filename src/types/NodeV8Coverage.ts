4b8c3fef98a13e03332c84585d01fa75a6d87213
jill64 <contact@jill64.dev>
2024-08-02 02:48:18 +0900

    fix: type linting

--- src/types/NodeV8Coverage.ts
+++ src/types/NodeV8Coverage.ts
@@ -1,4 +1,4 @@
-import { isNodeV8Coverage } from '../utils/isNodeV8Coverage.js'
+import type { isNodeV8Coverage } from '../utils/isNodeV8Coverage.js'
 import { GuardType } from './GuardType.js'
 
 export type NodeV8Coverage = GuardType<typeof isNodeV8Coverage>
